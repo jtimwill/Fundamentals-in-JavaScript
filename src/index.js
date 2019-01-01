29d769efec5d496750c714122306471aab871058
Tim Williams <jtimwill@gmail.com>
2019-01-01 10:35:36 -0500

    Revise Bootstrap dependencies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './custom.css'
+import './custom.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
