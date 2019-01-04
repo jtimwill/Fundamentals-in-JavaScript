24c83f79eb62954a4b76f4e2788547eae475394a
Tim Williams <jtimwill@gmail.com>
2019-01-03 19:17:15 -0500

    Begin adding content to site

--- src/utilities/data_structures.js
+++ src/utilities/data_structures.js
@@ -6,7 +6,65 @@ export default {
       name: "Arrays and Strings",
       language: "js",
       tabs: [
-              {name: "Question", data: "Answer"}
+              {
+                name: "Question",
+                data:
+`// Source:
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Indexed_collections#Array_object
+
+// Array JS
+let colors = ['red', 'blue']; // Instantiation
+colors[0]; //=> First item
+colors[colors.length - 1]; //=> Last item
+colors.forEach((item, index, array) => { // Loop over array
+  console.log(item, index);
+});
+colors.push('green'); // Add item to end
+colors.pop(); // Remove last item
+colors.unshift('white'); // Add item to front
+colors.shift(); // Remove item from front
+color.indexOf('red'); // Find index of item
+color.splice(pos, n); // Remove item(s) from index position(s)
+colors.slice(); // Shallow copy
+
+// Array Methods (they all appear to mutate the caller)
+concat() // joins two arrays and returns a new array.
+join(deliminator = ',') // joins all elements of an array into a string.
+reverse() // transposes the elements of an array: the first array element
+          // becomes the last and the last becomes the first. *Mutates Caller*
+sort([compareFunction]) // sorts the elements of an array.
+find(callback[, thisArg]) // returns a value in the array, if an element in the
+                          // array satisfies the provided testing function.
+                          // Otherwise undefined is returned.
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Text_formatting#String_objects
+// String JS
+charAt, charCodeAt, codePointAt // Return the character or character code at the
+                                // specified position in string.
+indexOf, lastIndexOf // Return the position of specified substring in the string
+                     // or last position of specified substring, respectively.
+startsWith, endsWith, includes // Returns whether or not the string starts, ends
+                               // or contains a specified string.
+concat // Combines the text of two strings and returns a new string.
+fromCharCode, fromCodePoint // Constructs a string from the specified sequence
+                            // of Unicode values. This is a method of the String
+                            // class, not a String instance.
+split // Splits a String object into an array of strings by separating the
+      // string into substrings.
+slice // Extracts a section of a string and returns a new string.
+substring, substr // Return the specified subset of the string, either by
+                  // specifying the start and end indexes or the start index and
+                  // a length.
+match, replace, search //Work with regular expressions.
+toLowerCase, toUpperCase // Return the string in all lowercase or all uppercase,
+                         // respectively.
+normalize // Returns the Unicode Normalization Form of the calling string value.
+repeat // Returns a string consisting of the elements of the object repeated the
+       // given times.
+trim	// Trims whitespace from the beginning and end of the string.
+`
+              }
             ]
     },
     {
--- src/utilities/primitive_types.js
+++ src/utilities/primitive_types.js
@@ -5,19 +5,90 @@ export default {
       id: 0,
       name: "Bit manipulation",
       language: "js",
-      tabs: [{name: "Question", data: "Answer"}]
+      tabs: [{
+        name: "Question",
+        data:
+`// Source: https://en.wikipedia.org/wiki/Mask_(computing)
+1001 & 0010 //=> 0000 (Query status of single bit. Returns 0 iff bit is off)
+1001 | 1111 //=> 1111 (Mask on)
+1001 & 1111 //=> 0000 (Mask off)
+1001 ^ 1111 //=> 0110 (Toggle bit values)
+
+// Source: https://www.cheatography.com/miracoli/cheat-sheets/bitmanipulation/
+(x>>5) & 1   // Test if bit 5 is set (same as "query status")
+x |= (1<<5)  // Set bit 5
+x &= ~(1<<5) // Clear bit 5
+x ^= (1<­­<5)  // Toggle bit 5
+x & 1        // Check if integer is odd
+x & (x-1)    // Turn off rightmost 1-bit
+x & (-x)     // Isolate rightmost 1-bit
+x | (x-1)    // Right propagate the rightmost 1-bit
+x | (x+1)    // Turn on the rightmost 0-bit
+~x & (x+1)   // Isolate rightmost 0-bit
+x & (x+1)    // Right propagate the rightmost 0-bit
+x <<= 1      // Multiply by 2
+x >>= 1      // Divide by 2
+1 << n       // 2^n
+x = (x | ' ') // Convert letter to lowercase
+x = (x & '_') // Convert letter to uppercase
+a ^= b; b ^= a; a ^= b; // XOR swap
+(x << 4) | (x >> 4) // Swap nibbles: 0b11110000 => 0b00001111
+`
+             }]
     },
     {
       id: 1,
       name: "Type conversion",
       language: "js",
-      tabs: [{name: "Question", data: "Answer"}]
+      tabs: [{
+        name: "Question",
+        data:
+`// Sources:
+//  https://medium.freecodecamp.org/js-type-coercion-explained-27ba3d9a2839
+//  https://www.w3schools.com/js/js_type_conversion.asp
+
+// To String:
+String(123.11) //=> "123.11"
+(123.11).toString() //=> "123.11"
+123.11 + "" //=> "123.11"
+
+// To Number:
+parseFloat("123.11") //=> 123.11
+parseInt("123.99") //=> 123
+Number("123.11") //=> 123.11
++"123.11" //=> 123.11
+
+// To Boolean:
+Boolean(2) //=> true
+!!2 //=> true`
+      }]
     },
     {
       id: 2,
       name: "Type checking",
       language: "js",
-      tabs: [{name: "Question", data: "Answer"}]
+      tabs: [{
+        name: "Question",
+        data:
+`// Source: https://javascript.info/types
+
+// There are 7 basic types in JavaScript.
+number // for numbers of any kind: integer or floating-point.
+string // for strings. A string may have one or more characters, there’s no separate single-character type.
+boolean // for true/false.
+null // for unknown values – a standalone type that has a single value null.
+undefined // for unassigned values – a standalone type that has a single value undefined.
+object // for more complex data structures.
+symbol // for unique identifiers.
+
+// The typeof operator allows us to see which type is stored in the variable.
+// 1. Two forms:
+  typeof x
+  typeof(x)
+// 2. Returns a string with the name of the type, like "string".
+// 3. For null returns "object" – that’s an error in the language, it’s not an object in fact.
+`
+             }]
     },
     {
       id: 3,
